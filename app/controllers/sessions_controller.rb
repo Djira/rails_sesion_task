a25eb140adebf70a0337dd93d5fe9beca359c1a1
Djira <djcoully7@gmail.com>
2023-05-09 09:44:30 +0000

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       log_in(user)
       redirect_to tasks_path, notice: 'ログインしました'
     else
-      flash.now[:alert] = '有効なアカウントを入力してください'
+      flash.now[:alert] = 'メールアドレスまたはパスワードに誤りがあります'
       render :new
     end
   end
